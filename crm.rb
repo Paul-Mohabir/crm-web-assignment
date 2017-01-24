779143440e6526f9285ac5df3827e35d6a0f076f
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-01-24 14:41:52 -0500

    added the sub menu options and fake content for the names now will proceded to step 5

--- crm.rb
+++ crm.rb
@@ -1,6 +1,12 @@
+require_relative "contact"
 require 'sinatra'
 
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
 get '/' do
+  @crm_app_name = "Paul Mohabir"
   erb :index
 end
 
